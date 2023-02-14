6fccd644792aa97f426debbfa5376958a170d103
FaberJoo <sjdk0805@gmail.com>
2023-02-14 13:37:13 +0900

    [추가] 프레임워크

---
+++ src/main/java/com/codestates/todogithubtest/TodoGithubTestApplication.java
@@ -0,0 +1,13 @@
+package com.codestates.todogithubtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoGithubTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoGithubTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codestates/todogithubtest/TodoGithubTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codestates.todogithubtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoGithubTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
